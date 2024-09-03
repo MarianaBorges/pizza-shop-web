f6754bd05cc82c16a32bde1a007c6ad7abeb970b
Mariana Mariano Borges <mariannamarianoborges@gmail.com>
2024-09-03 09:18:28 -0300

    feat: add sign in and sign up pages

---
+++ src/routes.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './pages/_layout/app'
+import { AuthLayout } from './pages/_layout/auth'
+import { Dashboard } from './pages/app/dashboard'
+import { SignIn } from './pages/auth/signIn'
+import { SignUp } from './pages/auth/signUp'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
+  },
+])
