f546f972df5799341a9c5b4c31a478f53c3f84e8
Mariana Mariano Borges <mariannamarianoborges@gmail.com>
2024-09-23 20:01:59 -0300

    feat: add test config

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+})
